3a995367b82e92ae22bd96a314aa90c284ba178b
jasinco <dan640001@gmail.com>
2023-08-08 03:10:22 +0800

    YTDLP support

--- src/ytdlp/mod.rs
+++ src/ytdlp/mod.rs
@@ -9,6 +9,7 @@ pub fn get_audio_url(url:&str) -> result::Result<String, Box<dyn error::Error>>{
     let output = process::Command::new("./yt-dlp").args([url, "-x","--skip-download", "-j"]).output()?;
     let stro = String::from_utf8(output.stdout)?;
     let val:Value = serde_json::from_str(&stro)?;
+    // TODO: The error message of ytdlp
     let url = val.get("url").unwrap().as_str().unwrap().to_string();
     Ok(url)
 }
